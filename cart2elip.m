9e96d51b1751f3adcbb06c47665f75cbf3f7d36e
RaulMiranda02 <raul.miranda@estudiantat.upc.edu>
2025-12-17 13:24:45 +0100

    Funció per passar de coordenades cartesianes a elíptiques. Funció readGlonassData modificada.

---
+++ cart2elip.m
@@ -0,0 +1,22 @@
+function [data, err] = cart2elip(data)
+
+a = 6378.137;
+e = 0.081819;
+p = sqrt(data.position.x.^2 + data.position.y.^2);
+data.position.elip.long = atan(data.position.y./data.position.x);
+data.position.elip.lat = atan((data.position.z./(p))./(1-e^2));
+
+thresh = 10e-10;
+err = 10e9;
+it = 1;
+while (err > thresh)
+    lat = data.position.elip.lat;
+    N = a./sqrt(1-(e^2.*sin(lat).^2));
+    h = p/(cos(lat)) - N;
+    lat_prev = lat;
+    data.position.elip.lat = atan((data.position.z./p)./(1-e^2.*(N./(N+h))));
+    err(it) = max(data.position.elip.lat - lat_prev);
+    it = it + 1;
+end
+
+end
--- readGlonassData.m
+++ readGlonassData.m
@@ -22,27 +22,27 @@ function [leapSeconds, data] = readGlonassData(fileName)
         % Línia de PRN, temps ...
         a = fgetl(fid);
         data.PRN(k) = str2double(a(1:2));
-        data.time(k).year = str2double(a(4:5));
-        data.time(k).month = str2double(a(7:8));
-        data.time(k).day = str2double(a(10:11));
-        data.time(k).hour = str2double(a(13:14));
-        data.time(k).minute = str2double(a(16:17));
-        data.time(k).seconds = str2double(a(20:22));
+        data.time.year(k) = str2double(a(4:5));
+        data.time.month(k) = str2double(a(7:8));
+        data.time.day(k) = str2double(a(10:11));
+        data.time.hour(k) = str2double(a(13:14));
+        data.time.minute(k) = str2double(a(16:17));
+        data.time.seconds(k) = str2double(a(20:22));
 
         x = fgetl(fid);
-        data.position(k).x = str2double(x(4:22));
-        data.velocity(k).x = str2double(x(23:41));
-        data.acceleration(k).x = str2double(x(42:60));
+        data.position.x(k) = str2double(x(4:22));
+        data.velocity.x(k) = str2double(x(23:41));
+        data.acceleration.x(k) = str2double(x(42:60));
 
         y = fgetl(fid);
-        data.position(k).y = str2double(y(4:22));
-        data.velocity(k).y = str2double(y(23:41));     
-        data.acceleration(k).y = str2double(y(42:60));
+        data.position.y(k) = str2double(y(4:22));
+        data.velocity.y(k) = str2double(y(23:41));     
+        data.acceleration.y(k) = str2double(y(42:60));
 
         z = fgetl(fid);
-        data.position(k).z = str2double(z(4:22));      
-        data.velocity(k).z = str2double(z(23:41));
-        data.acceleration(k).z = str2double(z(42:60));
+        data.position.z(k) = str2double(z(4:22));      
+        data.velocity.z(k) = str2double(z(23:41));
+        data.acceleration.z(k) = str2double(z(42:60));
 
     end
 
